f91c094f2f419c717068dc6af8eee08f8b789be1
Adrian <adrian@Adrians-Mac.local>
2018-10-18 13:15:38 +0200

    1.  Views

---
+++ DemoFiestaMediaNaranja/BIrthdayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BIrthdayViewController.swift
+//  DemoFiestaMediaNaranja
+//
+//  Created by Adrian on 18/10/2018.
+//  Copyright © 2018 Adrian. All rights reserved.
+//
+
+import UIKit
+
+class BIrthdayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
